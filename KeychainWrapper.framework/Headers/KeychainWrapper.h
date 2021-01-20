d1fab796753ad86b9c169d93af7d8075c98ed73d
Artem Melnyk <artem.melnyk.e@gmail.com>
2021-01-20 18:25:08 +0200

    Framework file

---
+++ KeychainWrapper.framework/Headers/KeychainWrapper.h
@@ -0,0 +1,18 @@
+//
+//  KeychainWrapper.h
+//  KeychainWrapper
+//
+//  Created by Artem Melnyk on 20.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KeychainWrapper.
+FOUNDATION_EXPORT double KeychainWrapperVersionNumber;
+
+//! Project version string for KeychainWrapper.
+FOUNDATION_EXPORT const unsigned char KeychainWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainWrapper/PublicHeader.h>
+
+
